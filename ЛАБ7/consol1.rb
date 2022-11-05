b9a7afed5ef71d2198e3f394ea53323bc5a24b77
HiggsBison <1444666@gmail.com>
2022-11-05 15:38:32 +0300

    made 7 lab 1 part

---
+++ ЛАБ7/consol1.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative 'lab7_part1.rb'
+puts 'start'
+First.make_files
+puts 'Files were created.'
+First.show_files
+
+
+
+# begin
+#   print "Example: if file name is 'text.txt' just input 'text'\nInput file name without format: "
+#   file_line_reverse(gets.chomp)
+#   puts "Success! check file 'output.txt'"
+# rescue StandardError => e
+#   puts e.message
+# end
---
+++ ЛАБ7/lab7_part1.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# class to make files and show them
+class First
+  def self.make_files(*str)
+    if str.empty?
+        make_f
+    else
+        make_f(str)
+    end
+    make_g
+  end
+
+  def self.make_f(*str)
+    str = str.join unless str.empty?
+    file_f = File.new('F.txt', 'w')
+    str = (0...rand(10..30)).map { rand(-20..100) }.join(' ') if str.empty?
+    file_f.write(str)
+    file_f.close
+  end
+
+  def self.make_g
+    file_g = File.new('G.txt', 'w')
+
+    File.open('F.txt', 'r').readline.split(' ').map(&:to_i).each do |number|
+      file_g << "#{number} " if (number % 3).zero? && (number % 7 != 0)
+    end
+
+    file_g.close
+  end
+
+  def self.show_files
+    puts "\nFile F:"
+    file_f = File.open('F.txt', 'r')
+    puts file_f.readline
+    file_f.close
+
+    puts "\nFile G:"
+    file_g = File.open('G.txt')
+    puts file_g.readline
+    file_g.close
+  end
+end
