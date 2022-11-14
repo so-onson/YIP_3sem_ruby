b920b103c5b3e29e1df83b8351f909ee5108b3c1
HiggsBison <1444666@gmail.com>
2022-11-14 19:27:14 +0300

    reek and rubocop clear

--- ЛАБ6/test1.rb
+++ ЛАБ6/test1.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 require_relative 'lab6_part1'
 
 RSpec.describe First do
+  let(:part) { First.new }
 
-    let(:part) { First.new }
-
-    it 'should calculate correctly' do
-      expect(part.calculate(0.0001)).to be_within(1e-3).of(0.98999999)
-    end
-  
-    it 'should has correct number of iterations' do
-      part.calculate(0.0001)
-      expect(part.iter_count).to eq(100)
-    end
+  it 'should calculate correctly' do
+    expect(part.calculate(0.0001)).to be_within(1e-3).of(0.98999999)
   end
+
+  it 'should has correct number of iterations' do
+    part.calculate(0.0001)
+    expect(part.iter_count).to eq(100)
+  endr
+end
