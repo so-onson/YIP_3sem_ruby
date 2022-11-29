9f1e94bda31dc1f012510136d9ae2ee1365f8c64
HiggsBison <1444666@gmail.com>
2022-11-29 22:51:34 +0300

    capybara

--- ЛАБ8/lab8/spec/requests/example_spec.rb
+++ ЛАБ8/lab8/spec/requests/example_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Examples', type: :request do
   describe 'GET /input' do
     it 'returns http success' do
-      get '/example/input'
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
@@ -18,8 +18,6 @@ RSpec.describe 'Examples', type: :request do
     end
 
     context 'shouldn`t redirect and return http Error if' do
-      
-      
       it 'redirecting was without params' do
         post '/example/show'
         expect(response).to have_http_status(:redirect)
@@ -47,38 +45,37 @@ RSpec.describe 'Examples', type: :request do
     end
 
     context 'should return body contains' do
-    subject { post example_show_path, params: { num1: 17, num2: 68 }, xhr: true }
+      subject { post example_show_path, params: { num1: 17, num2: 68 }, xhr: true }
 
       it 'input 17, 68' do
         subject
-          expect(assigns(:result).flatten).to eq([1,	17, 51, 2, 17, 34, 3, 17, 17])
-
+        expect(assigns(:result).flatten).to eq([1,	17, 51, 2, 17, 34, 3, 17, 17])
       end
     end
   end
 
-  describe 'Capybara test case', type: :feature do
-    context 'Capybara test case' do
-  
-        it 'header' do
-          visit root_path
-          expect(page). to have_content 'Example'
-        end
-      end
-    end
+  # describe 'Capybara test case', type: :feature do
+  #   context 'Capybara test case' do
 
-  describe 'Selenium WebDriver open root_path and', type: :feature do
-    before(:each) do
-      @driver = Selenium::WebDriver.for :firefox
-      @base_url = 'https://google.com'
-      @accept_next_alert = true
-      @driver.manage.timeouts.implicit_wait = 30
-      @verification_errors = []
-    end
+  #       it 'header' do
+  #         visit root_path
+  #         expect(page). to have_content 'Example'
+  #       end
+  #     end
+  #   end
 
-    after(:each) do
-      @driver.quit
-      expect(@verification_errors.size).to be_zero
-    end
-  end
+  # describe 'Selenium WebDriver open root_path and', type: :feature do
+  #   before(:each) do
+  #     @driver = Selenium::WebDriver.for :firefox
+  #     @base_url = 'https://google.com'
+  #     @accept_next_alert = true
+  #     @driver.manage.timeouts.implicit_wait = 30
+  #     @verification_errors = []
+  #   end
+
+  #   after(:each) do
+  #     @driver.quit
+  #     expect(@verification_errors.size).to be_zero
+  #   end
+  # end
 end
